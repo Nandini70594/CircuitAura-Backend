f0cb1924a74fbced524905ffd87506b5a8b136e8
Nandini Rahane <nandinirahane89@gmail.com>
2026-01-11 00:36:05 +0530

    fix: CORS syntax error - add missing comma for .in domain

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const corsOptions = {
   origin: [
     "http://localhost:8080",
     "http://localhost:5173",
-    "https://circuitaura.netlify.app"
+    "https://circuitaura.netlify.app",
     "https://circuitaura.in"
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
